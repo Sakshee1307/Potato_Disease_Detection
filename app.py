c4e09d4c3df1f6f84b887a1ffa6d136b395c6eb8
SAKSHI DEEPAK JADHAV <132775197+Sakshee1307@users.noreply.github.com>
2024-06-02 17:31:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,29 @@
 import streamlit as st
-import numpy as np
-import tensorflow as tf
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the trained model
-model = tf.keras.models.load_model('my_model.keras')
+# Load the model
+model = load_model('my_model.keras')
 
-class_names = [ 'Healthy','Early_blight', 'Late_blight']
-
-# Function to make a prediction
-def prediction(img):
-    # Load the image and convert it to RGB if it's grayscale
-    my_image = image.load_img(img, target_size=(256, 256))
-    my_image = image.img_to_array(my_image)
-    if my_image.shape[-1] == 1:  # If the image has only one channel (grayscale)
-        my_image = np.concatenate([my_image] * 3, axis=-1)  # Convert to RGB
-
-    my_image = np.expand_dims(my_image, 0)
-
-    out = np.round(model.predict(my_image)[0], 2)
-    return out
-
-# Streamlit app UI
-st.title("Potato Disease Classification")
-st.write("Upload an image of a potato leaf to classify its disease status.")
+st.title('Potato Disease Classification')
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     # Display the uploaded image
-    image_data = uploaded_file.read()
-    st.image(image_data, caption='Uploaded Image', use_column_width=True)
+    image = plt.imread(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
 
     # Make prediction
-    with open("temp_image.jpg", "wb") as f:
-        f.write(image_data)
+    img_array = image.load_img(uploaded_file, target_size=(256, 256))
+    img_array = image.img_to_array(img_array)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-    prediction_result = prediction("temp_image.jpg")
+    prediction = model.predict(img_array)
+    class_names = ['Early Blight', 'Healthy', 'Late Blight']
 
-    # Display prediction result
-    fig, ax = plt.subplots()
-    ax.barh(class_names, prediction_result, color='lightgray', edgecolor='red', linewidth=1, height=0.5)
-    for index, value in enumerate(prediction_result):
-        ax.text(value/2 + 0.1, index, f"{100*value:.2f}%", fontweight='bold')
-    ax.set_yticks([0, 1, 2])
-    ax.set_yticklabels(class_names, fontweight='bold', fontsize=14)
-    ax.set_xticks([])
-    st.pyplot(fig)
+    st.write("Prediction:")
+    for i in range(len(class_names)):
+        st.write(f"{class_names[i]}: {prediction[0][i]}")
